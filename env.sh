d7f471b6cb670a971449a140e308851f7eab9d1a
christopher-henderson <chris@chenderson.org>
2019-01-20 14:19:59 -0800

    simplifying the repo

--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-P=/Users/chris/Documents/Contracting/mozilla/CACop/dist/Debug/bin
-export PATH=$PATH:$P
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/chris/Documents/Contracting/mozilla/CACop/dist/Debug/lib
